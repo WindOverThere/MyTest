f65316cb80521548cfb8ba72624f1a1528546ca2
WindOverThere <gchengjunfeng@gmail.com>
2017-12-22 14:10:11 +0800

    nodeTest

--- nodeTest/server.js
+++ nodeTest/server.js
@@ -1,4 +1,4 @@
-'use strict';
+// 'use strict';
 // var http = require('http');
 // http.createServer(function (request, response){
 //     console.log(request)
@@ -8,21 +8,80 @@
 // console.log("Server running at http://127.0.0.1:8888/")
 
 
-var http = require("http")
-var url = require("url")
+// var http = require("http")
+// var url = require("url")
 
-function start(route) {
-	function onRequest(request, response){
-		var pathname = url.parse(request.url).pathname;
-		console.log("Request for " + pathname + " received.")
+// function start(route) {
+// 	function onRequest(request, response){
+// 		var pathname = url.parse(request.url).pathname;
+// 		console.log("Request for " + pathname + " received.")
 
-		route(pathname);
+// 		route(pathname);
 
-		response.writeHead(200, {"Content-Type": "text/plain"})
-		response.write("Hello World")
-		response.end()
+// 		response.writeHead(200, {"Content-Type": "text/plain"})
+// 		response.write("Hello World")
+// 		response.end()
+// 	}
+// 	http.createServer(onRequest).listen(8888)
+// 	console.log("Server has started.")
+// }
+// exports.start = start;
+
+
+
+// var fs = require('fs');
+// function copy(src, dst){
+// 	fs.createReadStreem(src).pipe(fs.createWriteStream(dst))
+// }
+// function main(argv) {
+// 	copy(argv[0], argv[1]);
+// }
+// main(process.argv.slice(2))
+
+// var bin = new Buffer([0x48, 0x65, 0x6c, 0x6c, 0x6c ]);
+// var dup = new Buffer(bin.length);
+// bin.copy(dup)
+// dup[0] = 0x68;
+// console.log(bin);
+// console.log(dup);
+
+
+
+// var rs = fs.creatReadStream(src);
+// rs.on('data', function (chunk){
+// 	rs.psuse();
+// 	doSomething(chunk, function (){
+// 		rs.resume()
+// 	})
+// })
+
+// rs.on('end', function (){
+// 	cleanUp();
+// })
+
+
+// function travel(dir, callback){
+// 	fs.readdirSync(dir).forEach(function (file){
+// 		var pathname = path.join(dir, file)
+// 		if(fs.statSync(pathname).isDirectory()){
+// 			travel(pathname, callback)
+// 		}else{
+// 			callback(pathname)
+// 		}
+// 	})
+// }
+
+
+function readText(pathname){
+	var bin = fs.readFileSync(pathname);
+	if(bin[0] === 0xEF && bin[1] === 0xBB && bin[2] === 0xBF){
+		bin = bin.slice(3)
 	}
-	http.createServer(onRequest).listen(8888)
-	console.log("Server has started.")
+	return bin.toString('utf-8')
 }
-exports.start = start;
+var iconv = require('iconv-lite')
+function readGBKText(pathname){
+	var bin = fs.readFileSync(pathname);
+
+	return iconv.decode(bin, 'gbk');
+}
